af15ff0bf6abdd36f5abbc5f4e8b90c83f82e2be
Juan Esteban <juanestban000@gmail.com>
2021-06-25 17:47:57 -0500

    Feat: missing put all animations to second spinner [breending-rhombus-spinner]

--- src/components/Molecules/Spinners/BreedingSpinner/BreedingSpinner.js
+++ src/components/Molecules/Spinners/BreedingSpinner/BreedingSpinner.js
@@ -1,10 +1,23 @@
 import defaultProps from '../../../../config/defaultProps'
-import { BreendingSpinnerContainer, RhombusContent } from './styles'
+import {
+  BreendingSpinnerContainer,
+  RhombusContent,
+  RhombusContentBig,
+} from './styles'
 
 export const BreedingSpinner = ({ size, color, duration } = defaultProps) => {
+  const rhombusArray = ['', '', '', '', '', '', '', '']
   return (
     <BreendingSpinnerContainer>
-      <RhombusContent />
+      {rhombusArray.map((_, index) => (
+        <RhombusContent
+          key={index}
+          size={size}
+          color={color}
+          duration={duration}
+        />
+      ))}
+      <RhombusContentBig size={size} color={color} duration={duration} />
     </BreendingSpinnerContainer>
   )
 }
--- src/components/Molecules/Spinners/BreedingSpinner/styles.js
+++ src/components/Molecules/Spinners/BreedingSpinner/styles.js
@@ -7,4 +7,42 @@ export const BreendingSpinnerContainer = styled.div`
   transform: rotate(45deg);
 `
 
-export const RhombusContent = styled.div``
+const sizeCSS = ({ size = 65, other = 7.5 }) => `calc(${size}px / ${other})`
+const sizePositionCSS = ({ size = 65, other = 2.3077 }) =>
+  `calc(${size}px / ${other})`
+const durationCSS = ({ duration = 2000 }) => `${duration}ms`
+const bgCSS = ({ color = '#ff1d5e' }) => color
+
+export const RhombusContent = styled.div`
+  width: ${sizeCSS};
+  height: ${sizeCSS};
+  position: absolute;
+  animation-duration: ${durationCSS};
+  top: ${sizePositionCSS};
+  left: ${sizePositionCSS};
+  background-color: ${bgCSS};
+  box-sizing: border-box;
+  animation-iteration-count: infinite;
+
+  &:nth-child(2n + 0) {
+    margin-right: 0;
+  }
+`
+
+export const RhombusContentBig = styled.div`
+  width: ${({ size }) => sizeCSS({ size, other: 3 })};
+  height: ${({ size }) => sizeCSS({ size, other: 3 })};
+  animation-duration: ${durationCSS};
+  top: ${({ size }) => sizePositionCSS({ size, other: 3 })};
+  left: ${({ size }) => sizePositionCSS({ size, other: 3 })};
+  background-color: ${bgCSS};
+  animation-delay: 500ms;
+  box-sizing: border-box;
+  animation: breeding-rhombus-spinner-big 2s infinite;
+
+  @keyframes breeding-rhombus-spinner-big {
+    50% {
+      transform: scale(0.5);
+    }
+  }
+`
