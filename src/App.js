0d3def45168d85a93a3ac9ed6a752f4d3cb1bbd4
Juan Esteban <juanestban000@gmail.com>
2021-06-28 21:22:41 -0500

    Fix: error with gh-pages and react-router-dom with the path from browser and the navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import HomePage from './pages/Home'
 import NotFoundPage from './pages/NotFound'
